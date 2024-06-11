675dc86724eeecd3b45835b858e0ac9b6c9835ff
ndermer1 <dermernathan@gmail.com>
2024-06-11 17:39:24 -0400

    Update Main.java
    Changed all "//" to File.Seperator and some edge casing

--- Main.java
+++ Main.java
@@ -9,16 +9,21 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		//User input string for original file path
-		System.out.println("Enter file path: ");
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter file path: ");
 		String filePath = scan.next();
 		scan.close();
 		
 		//Creates variables for original file containing duplicates and creates the new file to put all the duplicates into.
 		//The new file will be put in the same directory as the original file
 		File originalFile = new File(filePath);
-		String newFileDirectory = filePath.substring(0, filePath.lastIndexOf("\\"));
-		String newFileName = "\\DuplicateSongs";
+		//If invalid path
+		if (!originalFile.exists() || !originalFile.isDirectory()) {
+            System.out.println("Invalid directory path.");
+            return;
+        }
+		String newFileDirectory = filePath.substring(0, filePath.lastIndexOf(File.separator));
+		String newFileName = File.separator + "DuplicateSongs";
 		File dupeFile = new File(newFileDirectory + newFileName);
 		dupeFile.mkdirs();
 		
@@ -29,14 +34,12 @@ public class Main {
 		
 		//Sorts through array finds all duplicates and moves them into the duplicates array list
 		for(int i=1; i<content.length; i++) {
-			//System.out.println(content[i]);
-			
 			String bufferTitle = shorten(content[i]);
 			String prevBufferTitle = shorten(content[i-1]);
 			
 			if(bufferTitle.equals(prevBufferTitle)) { //If the current index matches the previous index, move previous index
 				duplicates.add(content[i-1]);	
-			}else if(duplicates.size() > 0 && prevBufferTitle.equals(shorten(duplicates.get(duplicates.size()-1)))) { //if previous index matches last index moved, move previous index
+			}else if(!duplicates.isEmpty() && prevBufferTitle.equals(shorten(duplicates.get(duplicates.size()-1)))) { //if previous index matches last index moved, move previous index
 				duplicates.add(content[i-1]);	
 			}
 		}
@@ -64,6 +67,6 @@ public class Main {
         if (index == -1) {
             index = x.length();
         }
-		return (x.substring(0, index)).toLowerCase();
+		return (x.substring(0, index)).toLowerCase().trim();
 	}
 }
